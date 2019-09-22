42f1e9decd7febca29399bc51f630e70bdaf5cb8
ZMONX_GG <chumpitak@gmail.com>
2019-09-22 11:35:07 +0700

    edit maha-lap and test

--- maha-lap.js
+++ maha-lap.js
@@ -1,6 +1,55 @@
-class Mahalap {
-  static tamnaiwithday(day) {
-    return 2;
+class MahaLap {
+  static tamnaiWithDay(day) {
+    switch (day) {
+      case "อาทิตย์":
+        return 1;
+      case "จันทร์":
+        return 2;
+      case "อังคาร":
+        return 3;
+      case "พุธ":
+        return 4;
+      case "พฤหัสบดี":
+        return 5;
+      case "ศุกร์":
+        return 6;
+      case "เสาร์":
+        return 7;
+      case "สงกรานต์":
+        return 13;
+      case "ฮาโลวีน":
+        return 31;
+      case "คริสมาสต์":
+        return 25;
+      default:
+        return "Not Match";
+    }
   }
+
+  static tamnaiWithStory(story) {
+    const length = story.length;
+    var sum = 0;
+    for (var i = 1; i <= length; i++) {
+      sum = sum + i;
+    }
+    switch (sum) {
+      case 3:
+        return sum;
+      case 36:
+        return sum;
+      case 66:
+        return sum;
+      default:
+        "Not Match";
+    }
+  }
+
+  // static tamnaiWithStory(story) {
+  //     const n = story.length;
+  //     let sum = 0;
+  //     for (let i = 1; i <= n; i++) {
+  //       sum += i;
+  //     }
+  //     return sum % 100;
+  //   }
 }
-module.exports = Mahalap;
--- maha-lap.test.js
+++ maha-lap.test.js
@@ -1,7 +1,67 @@
-const Mahalap = require("./maha-lap");
-describe("Test Maha-lap", () => {
-  test("Test Tamnai with day", () => {
-    const result = Mahalap.tamnaiwithday("จันทร์");
+const MahaLap = require("./maha-lap");
+describe("Test Maha-Lap", () => {
+  test("Test Tamnai with day 1", () => {
+    const result = MahaLap.tamnaiWithDay("อาทิตย์");
+    expect(result).toEqual(1);
+  });
+
+  test("Test Tamnai with day 2", () => {
+    const result = MahaLap.tamnaiWithDay("จันทร์");
     expect(result).toEqual(2);
+  });
+
+  test("Test Tamnai with day 3", () => {
+    const result = MahaLap.tamnaiWithDay("อังคาร");
+    expect(result).toEqual(3);
+  });
+
+  test("Test Tamnai with day 4", () => {
+    const result = MahaLap.tamnaiWithDay("พุธ");
+    expect(result).toEqual(4);
+  });
+
+  test("Test Tamnai with day 5", () => {
+    const result = MahaLap.tamnaiWithDay("พฤหัสบดี");
+    expect(result).toEqual(5);
+  });
+
+  test("Test Tamnai with day 6", () => {
+    const result = MahaLap.tamnaiWithDay("ศุกร์");
+    expect(result).toEqual(6);
+  });
+
+  test("Test Tamnai with day 7", () => {
+    const result = MahaLap.tamnaiWithDay("เสาร์");
+    expect(result).toEqual(7);
+  });
+
+  test("Test Tamnai with day 13", () => {
+    const result = MahaLap.tamnaiWithDay("สงกรานต์");
+    expect(result).toEqual(13);
+  });
+
+  test("Test Tamnai with day 31", () => {
+    const result = MahaLap.tamnaiWithDay("ฮาโลวีน");
+    expect(result).toEqual(31);
+  });
+
+  test("Test Tamnai with day 25", () => {
+    const result = MahaLap.tamnaiWithDay("คริสมาสต์");
+    expect(result).toEqual(25);
+  });
+
+  test("Test Tamnai with story", () => {
+    const result = MahaLap.tamnaiWithStory("งู");
+    expect(result).toEqual(3);
+  });
+
+  test("Test Tamnai with story", () => {
+    const result = MahaLap.tamnaiWithStory("งูกับแมว");
+    expect(result).toEqual(36);
+  });
+
+  test("Test Tamnai with story", () => {
+    const result = MahaLap.tamnaiWithStory("Cat and Dog");
+    expect(result).toEqual(66);
   });
 });
